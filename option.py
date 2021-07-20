62d3531fa7132daff59b3b0589b211664cc25f41
rmool <armin2saadat@gmail.com>
2021-07-21 00:00:31 +0430

    fix Option class.

--- option.py
+++ option.py
@@ -1,8 +1,8 @@
 class Option:
-    def __init__(self, epoch=200, batch_size=128, learning_rate=0.01, lr_decay_rate=0.25, load_ckpt=None):
+    def __init__(self, epochs=200, batch_size=128, learning_rate=0.01, lr_decay_rate=0.25, load_ckpt=None):
         self.batch_size = batch_size
         self.num_workers = 1
-        self.epochs = epoch
+        self.epochs = epochs
         self.learning_rate = learning_rate
         self.lr_decay_rate = lr_decay_rate
         self.loader_shuffle = False
