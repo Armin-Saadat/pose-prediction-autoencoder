317167d8ac471ca66e9323d51a4dee082fd3ebd9
Nima Fathi <nimafathinimafathi@gmail.com>
2021-07-21 12:11:08 +0430

    Refactor dataloader

--- dataloader/de_global_dataloader.py
+++ dataloader/de_global_dataloader.py
@@ -3,13 +3,13 @@ import pandas as pd
 from ast import literal_eval
 
 
-class myDataset(torch.utils.data.Dataset):
+class DE_Global_DataLoader(torch.utils.data.Dataset):
     def __init__(self, args, dtype, fname):
-
         self.args = args
         self.dtype = dtype
+        self.fname = fname
         print("Loading", self.dtype)
-        sequence_centric = pd.read_csv(fname + self.dtype + ".csv")
+        sequence_centric = pd.read_csv('processed_csvs/' + self.fname + self.dtype + ".csv")
         df = sequence_centric.copy()
         for v in list(df.columns.values):
             print(v + ' loaded')
@@ -35,11 +35,12 @@ class myDataset(torch.utils.data.Dataset):
         outputs.append(true_speed)
         outputs.append(obs)
         outputs.append(true)
+
         return tuple(outputs)
 
 
-def data_loader(args, data, fname):
-    dataset = myDataset(args, data, fname)
+def data_loader_de_global(args, data, fname):
+    dataset = DE_Global_DataLoader(args, data, fname)
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=args.batch_size, shuffle=args.loader_shuffle,
         pin_memory=args.pin_memory)
