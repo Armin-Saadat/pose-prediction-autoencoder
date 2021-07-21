466ccceed8c9a422206e38daf593740efac0cba3
Nima Fathi <nimafathinimafathi@gmail.com>
2021-07-21 12:27:58 +0430

    Refactor dataloader

--- dataloader/de_global_dataloader.py
+++ dataloader/de_global_dataloader.py
@@ -27,11 +27,11 @@ class DE_Global_DataLoader(torch.utils.data.Dataset):
     def __getitem__(self, index):
         seq = self.data.iloc[index]
         outputs = []
-        obs = torch.tensor([seq.Pose[i] for i in range(0, self.args.input, self.args.skip)])
+        obs = torch.tensor([seq.Pose[i] for i in range(0, self.args.input, self.args.skip)])[:, :2]
         obs_speed = (obs[1:] - obs[:-1])
-        outputs.append(obs_speed)
-        true = torch.tensor([seq.Future_Pose[i] for i in range(0, self.args.output, self.args.skip)])
+        true = torch.tensor([seq.Future_Pose[i] for i in range(0, self.args.output, self.args.skip)])[:, :2]
         true_speed = torch.cat(((true[0] - obs[-1]).unsqueeze(0), true[1:] - true[:-1]))
+        outputs.append(obs_speed)
         outputs.append(true_speed)
         outputs.append(obs)
         outputs.append(true)
--- dataloader/de_local_dataloader.py
+++ dataloader/de_local_dataloader.py
@@ -28,19 +28,31 @@ class DE_Local_DataLoader(torch.utils.data.Dataset):
         seq = self.data.iloc[index]
         outputs = []
         obs = torch.tensor([seq.Pose[i] for i in range(0, self.args.input, self.args.skip)])
-        obs_speed = (obs[1:] - obs[:-1])
-        outputs.append(obs_speed)
+        local_obs = self.create_local_var(obs)
+        obs_speed = (obs[1:, 2:] - obs[:-1, 2:])
+
         true = torch.tensor([seq.Future_Pose[i] for i in range(0, self.args.output, self.args.skip)])
-        true_speed = torch.cat(((true[0] - obs[-1]).unsqueeze(0), true[1:] - true[:-1]))
+        local_true = self.create_local_var(true)
+        true_speed = torch.cat(((local_true[0] - local_obs[-1]).unsqueeze(0), local_true[1:] - local_true[:-1]))
+        outputs.append(obs_speed)
         outputs.append(true_speed)
-        outputs.append(obs)
-        outputs.append(true)
+        outputs.append(local_obs)
+        outputs.append(local_true)
+
         if self.fname == "posetrack_":
             obs_mask = torch.tensor([seq.Mask[i] for i in range(0, self.args.output, self.args.skip)])
             true_mask = torch.tensor([seq.Future_Mask[i] for i in range(0, self.args.output, self.args.skip)])
             outputs.append(obs_mask)
             outputs.append(true_mask)
         return tuple(outputs)
+
+    def create_local_var(self, data):
+        neck_joint_data = data[:, :2]  # [16,2]
+        other_joints_data = data[:, 2:]  # [16,26]
+        for i, val in enumerate(other_joints_data):
+            for j in range(13):
+                val[2*j: 2*(j+1)] = torch.sub(val[2*j: 2*(j+1)], neck_joint_data)
+        return other_joints_data
 
 
 def data_loader_de_local(args, data, fname):
