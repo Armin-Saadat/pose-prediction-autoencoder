562cc0f002387112f932f1f2b54e5f633f662d39
rmool <armin2saadat@gmail.com>
2021-07-21 11:16:33 +0430

    add visualizer.py

--- utils.py
+++ utils.py
@@ -3,11 +3,10 @@ from models.lstm_vel_3dpw import LSTM_Vel_3dpw
 from models.de_global_posetrack import DE_Global_Posetrack
 import torch
 import torch.optim as optim
-import matplotlib
-import numpy as np
+
 import pandas as pd
 from ast import literal_eval
-import cv2
+
 
 
 def set_loader(opt):
@@ -137,9 +136,6 @@ def speed2pos3d(preds, obs_p):
     return pred_pos
 
 
-
-
-
 class myDataset_posetrack(torch.utils.data.Dataset):
     def __init__(self, args, dtype, fname):
         self.args = args
@@ -191,140 +187,9 @@ def data_loader_posetrack(args, data, fname):
     return dataloader
 
 
-def draw_keypoints_posetrack(outputs, image_in=None):
-    edges = [(0, 1), (0, 2), (0, 3), (2, 4), (3, 5), (4, 6), (5, 7), (2, 8), (3, 9), (8, 9), (8, 10), (9, 11), (10, 12),
-             (11, 13)]
-
-    image = np.zeros((1080, 1920, 3))
-
-    keypoints = outputs
-
-    keypoints = keypoints.reshape(-1, 2)
-    print(keypoints.shape)
-    for p in range(keypoints.shape[0]):
-        # draw the keypoints
-        if (not (keypoints[p, 0] <= 0 and keypoints[p, 1] <= 0)):
-            cv2.circle(image, (int(keypoints[p, 0]), int(keypoints[p, 1])),
-                       3, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-            cv2.putText(image, f"{p}", (int(keypoints[p, 0] + 10), int(keypoints[p, 1] - 5)), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (255, 0, 0), 1)
-
-    for ie, e in enumerate(edges):
-        # get different colors for the edges
-        rgb = matplotlib.colors.hsv_to_rgb([
-            ie / float(len(edges)), 1.0, 1.0
-        ])
-        rgb = rgb * 255
-        # print(keypoints[e, 0][0])
-        # join the keypoint pairs to draw the skeletal structure
-        if not ((keypoints[e, 0][0] <= 0 or keypoints[e, 1][0] <= 0) or (
-                keypoints[e, 0][1] <= 0 or keypoints[e, 1][1] <= 0)):
-            cv2.line(image, (keypoints[e, 0][0], keypoints[e, 1][0]),
-                     (keypoints[e, 0][1], keypoints[e, 1][1]),
-                     tuple(rgb), 2, lineType=cv2.LINE_AA)
-
-    return image
 
 
-def draw_keypoints_op(outputs):
-    edges = [(13, 15), (13, 11), (16, 14), (14, 12), (11, 12), (5, 11), (6, 12), (5, 6), (5, 7), (6, 8), (8, 10),
-             (7, 9),
-             (1, 2), (0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (4, 6)]
 
-    #     print(outputs.shape)
-    image = np.zeros((1080, 1920, 3))
-    for i in range(len(outputs)):
-        keypoints = outputs[i]
-        keypoints = keypoints.reshape(-1, 2)
-        for p in range(keypoints.shape[0]):
-            if (not (keypoints[p, 0] == 0 and keypoints[p, 1] == 0)):
-                cv2.circle(image, (int(keypoints[p, 0]), int(keypoints[p, 1])),
-                           3, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-        for ie, e in enumerate(edges):
-            # get different colors for the edges
-            rgb = matplotlib.colors.hsv_to_rgb([
-                ie / float(len(edges)), 1.0, 1.0
-            ])
-            rgb = rgb * 255
-
-            if not ((keypoints[e, 0][0] == 0 and keypoints[e, 1][0] == 0) or (
-                    keypoints[e, 0][1] == 0 and keypoints[e, 1][1] == 0)):
-                cv2.line(image, (int(keypoints[e, 0][0]), int(keypoints[e, 1][0])),
-                         (int(keypoints[e, 0][1]), int(keypoints[e, 1][1])),
-                         tuple(rgb), 2, lineType=cv2.LINE_AA)
-        # else:
-        #     continue
-    #     image=cv2.resize(image,(1280,720))
-    return image
-
-
-def draw_keypoints(outputs):
-    edges = [(13, 15), (13, 11), (16, 14), (14, 12), (11, 12), (5, 11), (6, 12), (5, 6), (5, 7), (6, 8), (8, 10),
-             (7, 9),
-             (1, 2), (0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (4, 6)]
-
-    #     print(outputs.shape)
-    image = np.zeros((1080, 1920, 3))
-    for i in range(len(outputs)):
-        keypoints = outputs[i]
-        keypoints = keypoints.reshape(-1, 2)
-        for p in range(keypoints.shape[0]):
-            if (not (keypoints[p, 0] == 0 and keypoints[p, 1] == 0)):
-                cv2.circle(image, (int(keypoints[p, 0]), int(keypoints[p, 1])),
-                           3, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-        for ie, e in enumerate(edges):
-            # get different colors for the edges
-            rgb = matplotlib.colors.hsv_to_rgb([
-                ie / float(len(edges)), 1.0, 1.0
-            ])
-            rgb = rgb * 255
-
-            if not ((keypoints[e, 0][0] == 0 and keypoints[e, 1][0] == 0) or (
-                    keypoints[e, 0][1] == 0 and keypoints[e, 1][1] == 0)):
-                cv2.line(image, (int(keypoints[e, 0][0]), int(keypoints[e, 1][0])),
-                         (int(keypoints[e, 0][1]), int(keypoints[e, 1][1])),
-                         tuple(rgb), 2, lineType=cv2.LINE_AA)
-
-    return image
-
-
-def draw_keypoints_op(outputs, image_in=None):
-    edges = [(0, 1), (1, 2), (2, 3), (3, 4), (1, 5), (5, 6), (6, 7), (1, 8), (8, 9), (8, 12), (9, 10), (10, 11),
-             (12, 13), (13, 14), (11, 24), (11, 22), (22, 23), (14, 21), (14, 19), (19, 20), (0, 15), (0, 16), (15, 17),
-             (16, 18)]
-
-    image = np.zeros((1080, 1920, 3))
-    #     for i in range(len(outputs)):
-    keypoints = outputs
-
-    keypoints = keypoints.reshape(-1, 2)
-    print(keypoints.shape)
-    for p in range(keypoints.shape[0]):
-        # draw the keypoints
-        if (not (keypoints[p, 0] <= 0 and keypoints[p, 1] <= 0)):
-            cv2.circle(image, (int(keypoints[p, 0]), int(keypoints[p, 1])),
-                       3, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-    #               cv2.putText(image, f"{p}", (int(keypoints[p, 0]+10), int(keypoints[p, 1]-5)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-
-    for ie, e in enumerate(edges):
-        # get different colors for the edges
-        rgb = matplotlib.colors.hsv_to_rgb([
-            ie / float(len(edges)), 1.0, 1.0
-        ])
-        rgb = rgb * 255
-
-        # join the keypoint pairs to draw the skeletal structure
-        if not ((keypoints[e, 0][0] <= 0 and keypoints[e, 1][0] <= 0) or (
-                keypoints[e, 0][1] <= 0 and keypoints[e, 1][1] <= 0)):
-            cv2.line(image, (keypoints[e, 0][0], keypoints[e, 1][0]),
-                     (keypoints[e, 0][1], keypoints[e, 1][1]),
-                     tuple(rgb), 2, lineType=cv2.LINE_AA)
-
-    return image
 
 
 class AverageMeter(object):
