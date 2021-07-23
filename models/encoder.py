257aaa8b126f0c96309828002ea32cf3946189b8
rmool <armin2saadat@gmail.com>
2021-07-23 12:13:42 +0430

    debug encoder.

--- models/encoder.py
+++ models/encoder.py
@@ -6,8 +6,8 @@ class Encoder(nn.Module):
         super().__init__()
 
         self.args = args
-        self.rnns = nn.LSTM(input_size=input_size, hidden_size=args.hidden_size, num_layers=self.args.n_layers,
-                            dropout=dropout)
+        self.rnn = nn.LSTM(input_size=input_size, hidden_size=args.hidden_size, num_layers=self.args.n_layers,
+                           dropout=dropout)
 
         self.dropout = nn.Dropout(dropout)
 
